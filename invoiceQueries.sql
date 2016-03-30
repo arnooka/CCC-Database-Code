d9b58ff90617dd0e39a7ec22a87200fd78d37be5
Avinash Nooka <avinash.nooka@huskers.unl.edu>
2016-03-30 13:49:05 -0500

    Update invoiceQueries.sql

--- invoiceQueries.sql
+++ invoiceQueries.sql
@@ -8,39 +8,34 @@ select * from persons;
 select email from emails  where personID = (select personID from persons where personCode = '111'); 
 
 ##3. A query to add an email to a specific person
-
 insert into emails (personID, email)
-	  values ((select personID from persons where personCode = '111'),('ThecoolestDoc@tardis.g'));
+	values ((select personID from persons where personCode = '111'),('ThecoolestDoc@tardis.g'));
 
 ##4. A query to change the email address of a given email record
-
- set sql_safe_updates = 0;
+set sql_safe_updates = 0;
 update emails set email = 'Somethingelse@gmail.com' where email = 'ThecoolestDoc@tardis.g';
 
 ##5. A query (or series of queries) to remove a given person record
-
-delete from emails	where personID = (select personID from persons where personCode = 123);
+delete from emails where personID = (select personID from persons where personCode = 123);
 update customers set personID = null where personID = (select personID from persons where personCode = 123);
 delete from persons where personCode = 123;
 set sql_safe_updates = 1;
-
 
 ##6. A query to create a person record
 insert into persons (personCode, personFirstName, personLastName, addressID) 
 	values ("blah5","Steve","Jobbers","7");    
 
-
 ##7. A query to get all the products in a particular invoice
-select i.invoiceCode, p.productCode from invoices as i join invoiceProducts as p on i.invoiceID = p.invoiceID where i.invoiceCode = 'INV001';
-
+select i.invoiceCode, p.productCode from invoices as i
+	join invoiceProducts as p on i.invoiceID = p.invoiceID where i.invoiceCode = 'INV001';
 
 ##8. A query to get all the invoices of a particular customer
-select invoiceCode, c.customerName from invoices as i join customers as c on i.customerID = c.customerID where c.customerCode = 'C001';
+select invoiceCode, c.customerName from invoices as i 
+	join customers as c on i.customerID = c.customerID where c.customerCode = 'C001';
 select * from products;
 
-
 ##9. A query to create a new product record
-insert into products(prodCode, prodType, prodName, 	ppu, fee, hourly, yearly, personID) 
+insert into products(prodCode, prodType, prodName, ppu, fee, hourly, yearly, personID) 
 	values ('Han','C','shipment', 0, 150, 15,0, null); 
 
 ##10. A query to create a new invoice record
@@ -60,15 +55,17 @@ select c.customerName, count(*) from invoices as i join customers as c on i.cust
 	group by c.customerCode having count(*) >= 1;
 
 ##13. A query to find the total number of invoices for each salesperson
-select p.personFirstName, p.personLastName, count(*) from invoices as i join persons as p on i.personID = p.personID group by i.personID;
+select p.personFirstName, p.personLastName, count(*) from invoices as i
+	join persons as p on i.personID = p.personID group by i.personID;
 
 ##14. A query to find the total number of invoices that include a particular product
-select p.productCode, count(*) as 'invoices containing product' from invoices as i join invoiceProducts as p on i.invoiceID = p.invoiceID where p.productCode = 'c3p0'; 
+select p.productCode, count(*) as 'invoices containing product' from invoices as i
+	join invoiceProducts as p on i.invoiceID = p.invoiceID where p.productCode = 'c3p0'; 
 
 ##15. A query to find the total cost (excluding fees and taxes) of all equipment in each invoice
 select i.invoiceCode, sum(ip.units * p.ppu) as 'invoice equipment price' from invoices as i 
 	join invoiceProducts as ip on i.invoiceID = ip.invoiceID 
-    join products as p on ip.productID = p.productID;
+	join products as p on ip.productID = p.productID;
 
 ##16. A query to detect invalid data in invoices as follows. In a single invoice, a particular
 	##equipment product should only appear once (since any number of units can be consolidated
@@ -76,12 +73,12 @@ select i.invoiceCode, sum(ip.units * p.ppu) as 'invoice equipment price' from in
 	##instances of the same equipment product.
 select i.invoiceCode as 'invoice with conflicts', ip.productCode, count(*) as 'Times product appears' from invoices as i 
 	join invoiceProducts as ip on i.invoiceID = ip.invoiceID 
-    join products as p on ip.productID = p.productID
-    where p.prodType = 'E' group by ip.invoiceID, p.prodCode having (count(*)>1);  
+	join products as p on ip.productID = p.productID
+	where p.prodType = 'E' group by ip.invoiceID, p.prodCode having (count(*)>1);  
   
-## This one will find any duplicates     
-##select i.invoiceCode as 'invoice with conflicts', ip.productCode, count(*) as 'Times product appears' from invoices as i join  
-	##invoiceProducts as ip on i.invoiceID = ip.invoiceID 
+## This query will find any duplicates     
+##select i.invoiceCode as 'invoice with conflicts', ip.productCode, count(*) as 'Times product appears' from invoices as i
+	##join invoiceProducts as ip on i.invoiceID = ip.invoiceID 
 	##group by i.invoiceCode, ip.productCode having count(*)>1;
     
 ##17. Write a query to detect a possible conflict of interest as follows. No distinction is
@@ -91,5 +88,5 @@ select i.invoiceCode as 'invoice with conflicts', ip.productCode, count(*) as 'T
 select invoiceCode as 'conflicting invoices' from invoices as i 
 	join customers as c on c.customerCode = i.customerCode 
 	join persons as p on i.personID = p.personID
-    where i.personID = c.personID;
+	where i.personID = c.personID;
     
