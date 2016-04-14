5eeeb2e48cd3f1ebb1b8455f03895da2501e0b4d
Avinash Nooka <avinash.nooka@huskers.unl.edu>
2016-04-14 17:08:53 -0500

    Delete DatabaseInfo.java

--- src/DatabaseInfo.java
+++
@@ -1,7 +0,0 @@
-
-public class DatabaseInfo {
-
-	public static final String url = "jdbc:mysql://cse.unl.edu/vstanek";
-	public static final String username = "vstanek";
-	public static final String password = "mysqlpassword";
-}
